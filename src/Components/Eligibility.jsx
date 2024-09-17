2943a952d70b1223311ac9a504d44ca503f0771b
iamkodenaime <creativedannyk@gmail.com>
2024-09-17 15:36:13 +0100

    eligibility form added

---
+++ src/Components/Eligibility.jsx
@@ -0,0 +1,248 @@
+import React from 'react'
+import '../index.css'
+
+import  { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const Eligibility = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6436wtt', 'template_03g5tg6', form.current, 'ixOpuARCs9lBJOb0F')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset()
+
+  };
+
+  return (
+    <section >
+            
+            <form action="" class="eligibility-form">
+
+                <div class="input-wrapper">
+                <label for="name" class="input-label">Name *</label>
+
+                <input type="text" name="name" id="name" required placeholder="Type Name" class="input-field" />
+                </div>
+
+                <div class="input-wrapper">
+                <label for="email" class="input-label">Email Address *</label>
+
+                <input type="email" name="email" id="email" required placeholder="Type Email Address" class="input-field" />
+                </div>
+
+                <div class="input-wrapper">
+                <label for="email" class="input-label">Country Of Residence *</label>
+
+                <input type="text" name="name" id="name" required placeholder="Type your country" class="input-field" />
+                </div>
+
+                <div class="input-wrapper">
+                <label for="phone" class="input-label">Phone</label>
+
+                <input type="tel" name="phone" id="phone" placeholder="Type Phone Number" class="input-field" />
+                </div>
+
+              
+                <div className="wraps">
+                    <p>Years of Experience</p>
+                    <div className="checker">
+                        <input type="checkbox" id='check1'/>
+                        <label for="check1">below 5</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" id='check2'/>
+                        <label for="check2">5-10</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" id='check3'/>
+                        <label for="check3">10+</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" id='check4'/>
+                        <label for="check4">15+</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" id='check5'/>
+                        <label for="check5">20 and above</label>
+                    </div>
+                </div>
+
+                <div className="wraps">
+                    <p>Do you have any educational qualification?</p>
+                    <div className="checker">
+                        
+                        <input type="checkbox" />
+                        <label for="check1">Yes</label>
+                    </div>
+                    <div className="checker">
+                        
+                        <input type="checkbox" />
+                        <label for="check1">No</label>
+                    </div>
+                </div>
+
+                <div className="wraps">
+                    <p>Do you have any Certifications?</p>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check1">Yes</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check1">No</label>
+                    </div>
+                </div>
+
+                <div className="wraps">
+                    <p>If yes</p>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check1">National</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check2">International</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check3">Both</label>
+                    </div>
+                </div>
+
+                <div className="wraps">
+                    <p>Have you received any recognised awards</p>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check1">Yes</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check1">No</label>
+                    </div>
+                </div>
+
+                <div className="wraps">
+                    <p>If yes what kind?</p>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check1">National</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check2">International</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check3">Both</label>
+                    </div>
+                </div>
+
+                <div className="wraps">
+                    <p>Do you have any articles or publications?</p>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check1">Yes</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check2">No</label>
+                    </div>
+                </div>
+
+                <div className="wraps">
+                    <p>IF yes what Type?</p>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check1">Industry</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check2">Major Media</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check3">Both</label>
+                    </div>
+                </div>
+
+                <div className="wraps">
+                    <p>Industry Salary Range per Annum in USD</p>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check1">Below $40,000</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check2">$40,000 - $100,000</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check3">Above $100,000</label>
+                    </div>
+                </div>
+
+                <div className="wraps">
+                    <p>Have you delivered any Exhibitions /Workshops</p>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check1">Yes</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check2">No</label>
+                    </div>
+                </div>
+
+                <div className="wraps">
+                    <p>If yes what Category</p>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check1">National</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check2">International</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check3">Both</label>
+                    </div>
+                </div>
+
+                <div className="wraps">
+                    <p>Have you been a speaker on any Panel sessions?</p>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check1">Yes</label>
+                    </div>
+                    <div className="checker">
+                        <input type="checkbox" />
+                        <label for="check2">No</label>
+                    </div>
+                </div>
+
+                <div class="input-wrapper">
+                <label for="message" class="input-label">If yes, Tell us more *</label>
+
+                <textarea className='textarea'name="message" id="message" placeholder="Type Description" required
+                    class="input-field"></textarea>
+                </div>
+
+                <button type="submit" class="btn btns-primary">Submit</button>
+
+    </form>
+        
+    </section>
+  )
+}
+
+export default Eligibility
--- src/Pages/Info.jsx
+++ src/Pages/Info.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import Eligibility from '../Components/Eligibility'
 
 const Info = () => {
   return (
     <>
         <Navbar />
+        <Eligibility />
         <Footer />
     </>
   )
