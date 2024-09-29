97c5d9e3d71302567fa8c4a1533b574cf49fd574
iamkodenaime <creativedannyk@gmail.com>
2024-09-29 22:56:12 +0100

    updated images first batch

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -22,41 +22,18 @@ const Hero = () => {
 
               <a href="/info" class="btn btn-primary">Check Eligibility</a>
 
-              <a href="/home#contact" class="btn btn-outline">Let's Talk</a>
+              <a href="/home#contact" class="btn btn-outline">Consult Now</a>
 
             </div>
 
           </div>
 
-          <div class="hero-slider" data-slider>
-
-            <div class="slider-inner">
-              <ul class="slider-container" data-slider-container>
-
-                <li class="slider-item">
-
-                  <figure class="img-holder" width="575" height="550">
+          <div className="hero-pic">
+                  <figure class="hero-pic" width="575" height="550">
                     <img src={slide2}width="575" height="550" alt="" class="img-cover" />
                   </figure>
+          </div>             
 
-                </li>
-
-                
-
-              
-
-              </ul>
-            </div>
-
-            <button class="slider-btn prev" aria-label="slide to previous" data-slider-prev>
-              <ion-icon name="arrow-back"></ion-icon>
-            </button>
-
-            <button class="slider-btn next" aria-label="slide to next" data-slider-next>
-              <ion-icon name="arrow-forward"></ion-icon>
-            </button>
-
-          </div>
 
         </div>
       </section>
