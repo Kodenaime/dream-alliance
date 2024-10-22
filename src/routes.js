f22df493d2702163f95ab6f5774ebea6ea7a05b5
iamkodenaime <creativedannyk@gmail.com>
2024-10-22 12:30:11 +0100

    added packages for sitemap

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+
+
+const routes = [
+    { path: '/', exact: true, component: 'Home' },
+    { path: '/visa', component: 'Visa' },
+    { path: '/blogs', component: 'Blog' },
+    { path: '/info', component: 'Contact' },
+  ];
+  
+  module.exports = routes;
